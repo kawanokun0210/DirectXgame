ea947d6ed50d0389301e74c43aff346b30f37ff2
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-10-23 11:57:10 +0900

    バグった

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -11,17 +11,59 @@ void GameScene::Initialize(MyEngine* engine, DirectXCommon* dxCommon)
 	input_ = new Input();
 	input_->Initialize();
 
+	object_ = new Object();
+	object_->Initialize(dxCommon_,engine_);
+
+	for (int i = 0; i < 2; i++)
+	{
+		triangle_[i] = new Triangle();
+		triangle_[i]->Initialize(dxCommon_, engine_);
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+		sprite_[i] = new Sprite();
+		sprite_[i]->Initialize(dxCommon_, engine_);
+	}
+
+	sphere_ = new Sphere();
+	sphere_->Initialize(dxCommon_, engine_);
+
+	cameraTransform_ = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,-5.0f} };
+
 	soundDataHandle_ = sound_->LoadWave("Audio/Alarm01.wav");
+
+	objectTransform_ = { {0.4f,0.4f,0.4f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
+	objectMaterial_ = { 1.0f,1.0f,1.0f,1.0f };
+
+	objectDraw_ = true;
+
+	directionalLight_.color = { 1.0f,1.0f,1.0f,1.0f };
+	directionalLight_.direction = { 0.0f,-1.0f,0.0f };
+	directionalLight_.intensity = 1.0f;
+
+	texture_ = 0;
 
 }
 
 void GameScene::Update() {
 
+	Matrix4x4 sphereAffine = MakeAffineMatrix(sphereTransform_.scale, sphereTransform_.rotate, sphereTransform_.translate);
+	Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraTransform_.scale, cameraTransform_.rotate, cameraTransform_.translate);
+	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+	Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(dxCommon_->GetWin()->kClientWidth) / float(dxCommon_->GetWin()->kClientHeight), 0.1f, 100.0f);
+	Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix_, Multiply(viewMatrix, projectionMatrix));
+
+	worldMatrix_ = worldViewProjectionMatrix;
+	sphereMatrix_ = Multiply(sphereAffine, Multiply(viewMatrix, projectionMatrix));
+
+	directionalLight_.direction = Normalise(directionalLight_.direction);
+
 }
 
 void GameScene::Draw()
 {
-	
+	object_->Draw(objectMaterial_, objectTransform_, 0, cameraTransform_, directionalLight_);
 }
 
 void GameScene::Finalize()
