9856bbb69130c355b297e1cacc8f64c3b62e69e8
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-11-09 10:15:37 +0900

    シェーダー変更

--- Particle.PS.hlsl
+++ Particle.PS.hlsl
@@ -20,7 +20,7 @@ PixelShaderOutput main(VertexShaderOutput input)
 		discard;
 	}
 
-	output.color = gMaterial.color * textureColor;
+	output.color = gMaterial.color * textureColor * input.color;
 	
 	return output;
 }
