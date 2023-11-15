589f7a11dfcb955a7d9ab247e90ddf119e024fda
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-11-15 11:02:39 +0900

    何も変わらない

--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -7,6 +7,7 @@ VertexShaderOutput main(VertexShaderInput input, uint32_t instanceId : SV_Instan
 	VertexShaderOutput output;
 	output.position = mul(input.position, gParticle[instanceId].WVP);
 	output.texcoord = input.texcoord;
+	output.normal = normalize(mul(input.normal, (float32_t3x3)gParticle[instanceId].World));
 	output.color = gParticle[instanceId].color;
 	return output;
 }
