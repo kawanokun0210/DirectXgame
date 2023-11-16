9ceeecefc43f681b36ce303a1183a76172b5487b
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-11-16 11:09:58 +0900

    カメラ変更

--- scene/GamePlayScene.cpp
+++ scene/GamePlayScene.cpp
@@ -100,7 +100,7 @@ void GamePlayScene::Initialize(MyEngine* engine, DirectXCommon* dxCommon)
 		particles[index] = particle->MakeNewParticle(randomEngine);
 	}
 
-	cameraTransform_ = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,-20.0f} };
+	cameraTransform_ = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,-10.0f} };
 }
 
 void GamePlayScene::Update()
