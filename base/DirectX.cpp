8c35f324a08aebb6280d6ac2024215e3d3c2acf4
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-11-29 11:13:37 +0900

    分解能を上げた

--- base/DirectX.cpp
+++ base/DirectX.cpp
@@ -27,6 +27,9 @@ void DirectXCommon::Initialization(const wchar_t* title, int32_t backBufferWidth
 	//FPS固定初期化
 	InitializeFixFPS();
 
+	//システムタイマーの分解能を上げる
+	timeBeginPeriod(1);
+
 	// DXGIデバイス初期化
 	InitializeDXGIDevice();
 
@@ -473,7 +476,7 @@ void DirectXCommon::UpdateFixFPS() {
 	std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
 
 	//1/60秒(よりわずかに短い時間)経っていない場合
-	if (elapsed < kMinTime) {
+	if (elapsed < kMinCheckTime) {
 		//1/60秒経過するまで微小なスリープを繰り返す
 		while (std::chrono::steady_clock::now() - reference_ < kMinTime) {
 			//1マイクロ秒スリープ
