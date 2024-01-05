3a76081cbf12168a6a12dcdf435cc792c4093026
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2024-01-06 03:37:01 +0900

    l

--- scene/GameTitleScene.cpp
+++ scene/GameTitleScene.cpp
@@ -8,33 +8,12 @@ void GameTitleScene::Initialize(MyEngine* engine, DirectXCommon* dxCommon) {
 
 	time = 0;
 
-	//MT
-	rotation = MakeRotateAxisAngleQuaternion(Normalise(Vector3{ 1.0f,0.4f,-0.2f }), 0.45f);
-	Vector3 pointY = { 2.1f,-0.9f,1.3f };
-	rotateMatrix = MakeRotateMatrix(rotation);
-	rotateByQuaternion = RotateVector(pointY, rotation);
-	rotateByMatrix = VectorTransform(pointY, rotateMatrix);
-
 }
 
 void GameTitleScene::Update() {
 	input_->Update();
 
-	ImGui::Begin("MT4_01_04");
-	ImGui::Text("rotation");
-	ImGui::Text("%4.2f %4.2f %4.2f %4.2f", rotation.x, rotation.y, rotation.z, rotation.w);
-	ImGui::Text("rotateMatrix");
-	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3]);
-	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3]);
-	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3]);
-	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
-	ImGui::Text("rotateByQuaternion");
-	ImGui::Text("%4.2f %4.2f %4.2f", rotateByQuaternion.x, rotateByQuaternion.y, rotateByQuaternion.z);
-	ImGui::Text("rotateByMatrix");
-	ImGui::Text("%4.2f %4.2f %4.2f", rotateByMatrix.x, rotateByMatrix.y, rotateByMatrix.z);
-	ImGui::End();
-
-	if (input_->PushKey(DIK_SPACE)) {
+	if (input_->PushKey(DIK_RETURN)) {
 		sceneNo = PLAY;
 	}
 }
