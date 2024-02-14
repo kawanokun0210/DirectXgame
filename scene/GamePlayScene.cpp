00007b2d46ecd71ad01dbb6896d82e3fe92d4eee
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2024-02-14 16:15:26 +0900

    hennkou

--- scene/GamePlayScene.cpp
+++ scene/GamePlayScene.cpp
@@ -102,7 +102,6 @@ void GamePlayScene::Initialize(MyEngine* engine, DirectXCommon* dxCommon)
 
 	cameraTransform_ = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,-10.0f} };
 
-	cameraGPU.worldPosition = cameraTransform_.translate;
 }
 
 void GamePlayScene::Update()
