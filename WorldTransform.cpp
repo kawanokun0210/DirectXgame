04c5350bc04b2f2ec697e5e22b97b653477380e3
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2024-03-08 12:03:35 +0900

    一旦終わり

--- WorldTransform.cpp
+++ WorldTransform.cpp
@@ -1,1 +1,48 @@
 #include "WorldTransform.h"
+
+WorldTransform::~WorldTransform()
+{
+}
+
+void WorldTransform::Initialize(DirectXCommon* dxCommon)
+{
+	dxCommon_ = dxCommon;
+	matWorld_ = MakeIdentity4x4();
+	CreateConstBuffer();
+	Map();
+	TransferMatrix();
+
+}
+
+
+void WorldTransform::CreateConstBuffer()
+{
+	if (dxCommon_) {
+		constBuffer_ = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(TransformationMatrix));
+	}
+	else {
+		assert(dxCommon_);
+	}
+}
+
+void WorldTransform::Map()
+{
+	constBuffer_.Get()->Map(0, nullptr, reinterpret_cast<void**>(&constMap));
+}
+
+void WorldTransform::TransferMatrix()
+{
+	constMap->matWorld = matWorld_;
+}
+
+void WorldTransform::UpdateWorldMatrix()
+{
+	Matrix4x4 AffineMatrix = MakeAffineMatrix(scale_, rotation_, translation_);
+	matWorld_ = AffineMatrix;
+	//親があれば親のワールド行列を掛ける
+	if (parent_) {
+		matWorld_ = Multiply(matWorld_, parent_->matWorld_);
+	}
+
+	TransferMatrix();
+}
--- WorldTransform.h
+++ WorldTransform.h
@@ -1,5 +1,35 @@
 #pragma once
-class WorldTransform
-{
-};
+#include "DirectX.h"
+#include "MatrixCalculation.h"
+#include "Vertex.h"
 
+struct WorldTransform {
+	// 定数バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> constBuffer_;
+	// マッピング済みアドレス
+	TransformationMatrix* constMap = nullptr;
+	// ローカルスケール
+	Vector3 scale_ = { 1, 1, 1 };
+	// X,Y,Z軸回りのローカル回転角
+	Vector3 rotation_ = { 0, 0, 0 };
+	// ローカル座標
+	Vector3 translation_ = { 0, 0, 0 };
+	// ローカル → ワールド変換行列
+	Matrix4x4 matWorld_;
+	// 親となるワールド変換へのポインタ
+	const WorldTransform* parent_ = nullptr;
+
+	DirectXCommon* dxCommon_;
+
+	~WorldTransform();
+	
+	void Initialize(DirectXCommon* dxCommon);
+	
+	void CreateConstBuffer();
+	
+	void Map();
+
+	void TransferMatrix();
+
+	void UpdateWorldMatrix();
+};
