061350c5b659a231fe750a50643cce933bcd7769
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2024-03-27 10:40:27 +0900

    セーブ

--- scene/GameTitleScene.cpp
+++ scene/GameTitleScene.cpp
@@ -6,13 +6,50 @@ void GameTitleScene::Initialize(MyEngine* engine, DirectXCommon* dxCommon) {
 	input_ = Input::GetInstance();
 	input_->Initialize();
 
+	engine_->SettingTexture("Resource/uvChecker.png", 2);
+	engine_->SettingTexture("Resource/fence/fence.png", 3);
+	engine_->SettingTexture("Resource/circle.png", 4);
+	engine_->SettingTexture("Resource/particle.png", 5);
+
+	directionalLight_.color = { 1.0f,1.0f,1.0f,1.0f };
+	directionalLight_.direction = { 0.0f,-1.0f,0.0f };
+	directionalLight_.intensity = 1.0f;
+
+	object_[0] = new Object();
+	object_[0]->Initialize(dxCommon_, engine_, "Resource/fence", "fence.obj");
+
+	object_[1] = new Object();
+	object_[1]->Initialize(dxCommon_, engine_, "Resource/", "plane.obj");
+
+	for (int i = 0; i < 2; i++) {
+		objectTransform_[i] = { {0.4f,0.4f,0.4f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
+		objectMaterial_[i] = { 1.0f,1.0f,1.0f,1.0f };
+	}
+
+	objectTransform_[1] = { {0.4f,0.4f,0.4f},{0.0f,0.0f,0.0f},{1.0f,-1.0f,0.0f} };
+
+	particle_[0] = new Particle();
+	particle_[0]->Initialize(dxCommon_, engine_, "Resource/", "plane.obj");
+
+	particle_[1] = new Particle();
+	particle_[1]->Initialize(dxCommon_, engine_, "Resource/", "plane.obj");
+
+	std::mt19937 randomEngine(seedGenerator());
+
+	for (uint32_t index = 0; index < 10; ++index) {
+		particles[index] = particle_[0]->MakeNewParticle(randomEngine);
+		particles_[index] = particle_[1]->MakeNewParticle(randomEngine);
+	}
+
+	camera_ = new Camera();
+	camera_->Initialize(dxCommon_);
 
 }
 
 void GameTitleScene::Update() {
 	input_->Update();
 
-	
+
 
 	if (input_->TriggerKey(DIK_SPACE)) {
 		sceneNo = PLAY;
@@ -21,8 +58,24 @@ void GameTitleScene::Update() {
 
 void GameTitleScene::Draw() {
 
+	for (int i = 0; i < 2; i++) {
+		object_[i]->Draw(objectMaterial_[i], objectTransform_[i], 3, camera_, directionalLight_, true);
+	}
+
+	particle_[0]->Draw(&particles[0], 4, camera_);
+	particle_[1]->Draw(&particles_[0], 2, camera_);
+
 }
 
 void GameTitleScene::Finalize() {
+	for (int i = 0; i < 2; i++) {
+		object_[i]->Finalize();
+	}
+	delete object_[0];
+	delete object_[1];
 
+	delete particle_[0];
+	delete particle_[1];
+
+	delete camera_;
 }
--- scene/GameTitleScene.h
+++ scene/GameTitleScene.h
@@ -3,6 +3,10 @@
 #include "Input.h"
 #include "MatrixCalculation.h"
 #include "Quaternion.h"
+#include "Object.h"
+#include "Particle.h"
+#include <random>
+#include "../Camera.h"
 
 class GameTitleScene : public BaseScene 
 {
@@ -22,7 +26,24 @@ private:
 	Input* input_;
 
 	int time;
+	
+	//WP0
+	Particle* particle_[2];
 
+	Object* object_[2];
+	Transform objectTransform_[2];
+	Vector4 objectMaterial_[2];
+
+	ParticleData particles[10];
+	ParticleData particles_[10];
+
+	DirectionalLight directionalLight_;
+
+	Camera* camera_;
+
+	std::random_device seedGenerator;
+
+	//MT
 	Vector3 from0;
 	Vector3 to0;
 	Vector3 from1;
