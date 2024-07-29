44fa3962a82e07204d2b2bb48a216ff9e62384e7
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2024-07-29 11:12:25 +0900

    保存

--- LevelEditor.cpp
+++ LevelEditor.cpp
@@ -1,1 +1,7 @@
 #include "LevelEditor.h"
+
+void LevelEditor::LoadJsonFile() {
+	//連結してフルパスを得る
+	const std::string fullPath = "Resource/";
+
+}
--- LevelEditor.h
+++ LevelEditor.h
@@ -1,5 +1,35 @@
 #pragma once
+#include "DirectX.h"
+#include <dxcapi.h>
+#include "../String.h"
+#include "MatrixCalculation.h"
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Vector4.h"
+#include "../Vertex.h"
+#include "Triangle.h"
+#include "d3dx12.h"
+#include <vector>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include "MatrixCalculation.h"
+#include "Quaternion.h"
+#include <array>
+
 class LevelEditor
 {
+public:
+
+	void LoadJsonFile();
+
+	//void Update();
+
+	//void Draw();
+
+private:
+
+
+
 };
 
